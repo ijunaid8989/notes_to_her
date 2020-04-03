16eaa68bf7525ccd8180677c4a45bcf0f2201794
Junaid Farooq <junaid@evercam.io>
2020-04-03 22:37:53 +0500

    Updated Types with _NOTE.

--- src/types/index.js
+++ src/types/index.js
@@ -1,3 +1,6 @@
 export const SET_CURRENT_USER = "SET_CURRENT_USER"
 export const GET_ERRORS = "GET_ERRORS"
 export const CLEAR_ERRORS = 'CLEAR_ERRORS'
+export const GET_NOTES = 'GET_NOTES'
+export const NOTE_LOADING = 'NOTE_LOADING'
+export const ADD_NOTE = 'ADD_NOTE'
