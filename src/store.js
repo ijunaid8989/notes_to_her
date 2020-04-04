12f9766b960b469e1db400608b118b250fbbc81b
Junaid Farooq <junaid@evercam.io>
2020-04-04 17:41:08 +0500

    Added composeWithDevTools && Removed compose.

--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
 import {
   createStore,
-  applyMiddleware,
-  compose
+  applyMiddleware
 } from "redux"
+import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction'
 import thunk from "redux-thunk"
 import rootReducer  from "./reducers"
 
@@ -12,10 +12,7 @@ const initialState = {}
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeWithDevTools(applyMiddleware(...middleware))
 )
 
 export default store
